e08cf8fde783662ce67ba1e0100a6529a4bebe3b
GU33Y <uuao6989@gmail.com>
2026-06-18 18:33:33 +0200

    moved stuff around

--- src_exec/build.bat
+++ src_exec/build.bat
@@ -1,11 +1,11 @@
 @echo off
 cd /d %~dp0\src_exec
-clang++ -std=c++17 -O2 glld.cpp -o ..\glld.exe
+clang++ -std=c++17 -O2 glld.cpp -o ..\bin\glld.exe
 if errorlevel 1 exit /b %errorlevel%
-clang++ -std=c++17 -O2 gstdo.cpp -o ..\gstdo.exe
+clang++ -std=c++17 -O2 gstdo.cpp -o ..\bin\gstdo.exe
 if errorlevel 1 exit /b %errorlevel%
-clang++ -std=c++17 -O2 timer.cpp -o ..\timer.exe
+clang++ -std=c++17 -O2 timer.cpp -o ..\bin\timer.exe
 if errorlevel 1 exit /b %errorlevel%
-clang++ -std=c++17 -O2 gfree.cpp -o ..\gfree.exe
+clang++ -std=c++17 -O2 gfree.cpp -o ..\bin\gfree.exe
 if errorlevel 1 exit /b %errorlevel%
 echo Built Windows executables in %~dp0
